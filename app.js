ba21f969c8ca1595f44bd13261d6597d2356f206
Adi Jain <jainadi313@gmail.com>
2025-03-27 02:07:59 +0530

    improve mobile header and footer

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3010;
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
